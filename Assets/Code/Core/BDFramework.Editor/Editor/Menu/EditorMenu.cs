2ebf35cfe13dd9be5e088180c21cd24dbc281066
xiaofan <755737878@qq.com>
2018-03-26 18:17:47 +0800

    1.修改ui工作流编辑器

---
+++ Assets/Code/Core/BDFramework.Editor/Editor/Menu/EditorMenu.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+using BDFramework.Editor.UI;
+
+namespace BDFramework.Editor
+{
+    public class BDEditorMenu
+    {
+        
+        
+        [MenuItem("BDFrameWork工具箱/UI工作流/创建窗口", false, 1)]
+        public static void OpenEditorWindow_CreateUI()
+        {
+            var window =
+                (EditorWindows_CreateUI) EditorWindow.GetWindow(typeof(EditorWindows_CreateUI), false, "UI创建工具");
+            window.Show();
+        }
+        //
+
+        [MenuItem("BDFrameWork工具箱/资源打包/DLL打包", false, 1)]
+        public static void ExecuteBuildDLL()
+        {
+            var window =
+                (EditorWindow_ScriptBuildDll) EditorWindow.GetWindow(typeof(EditorWindow_ScriptBuildDll), false, "DLL打包工具");
+            window.Show();
+        }
+
+        [MenuItem("BDFrameWork工具箱/资源打包/AssetBundle打包", false, 2)]
+        public static void ExecuteAssetBundle()
+        {
+            var window =
+                (EditorWindw_GenAssetBundle) EditorWindow.GetWindow(typeof(EditorWindw_GenAssetBundle), false,
+                    "AB打包工具");
+            window.Show();
+        }
+
+        [MenuItem("BDFrameWork工具箱/资源打包/表格->生成Class", false, 3)]
+        public static void ExecuteGenTableCalss()
+        {
+            Excel2Code.GenCode();
+        }
+
+        [MenuItem("BDFrameWork工具箱/资源打包/表格->生成SQLite", false, 4)]
+        public static void ExecuteGenTable()
+        {
+            Excel2SQLite.GenSQLite();
+        }
+
+
+        [MenuItem("BDFrameWork工具箱/资源打包/一键打包", false, 51)]
+        public static void GenResouceall()
+        {
+            //Editor_VersionCtrl.GenTableCofig();
+        }
+        [MenuItem("BDFrameWork工具箱/资源打包/图片压缩",false,6)]
+        public static void ChangeTexture()
+        {
+            var window =(Editor_2ChangeTextureImporter)EditorWindow.GetWindow(typeof(Editor_2ChangeTextureImporter), false, "图片格式设置");
+            window.Show();
+        }
+    }
+}
---
+++ Assets/Code/Core/BDFramework.Editor/Editor/Resource/Excel2Code.cs
@@ -0,0 +1,160 @@
+﻿using System.CodeDom;
+using System.CodeDom.Compiler;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using BDFramework.ResourceMgr;
+using Game.Data;
+using LitJson;
+using UnityEditor;
+using UnityEditor.Graphs;
+using UnityEngine;
+
+namespace BDFramework.Editor
+{
+    static public class Excel2Code
+    {
+
+        public static void GenCode()
+        {
+            var tablePath = Path.Combine(Application.dataPath, FrameDataHelper.FrameWorkSetting.EditorTablePath);
+            var tableDir = Path.GetDirectoryName(tablePath);
+            var xlslFiles = Directory.GetFiles(tableDir, "*.xlsx", SearchOption.AllDirectories);
+
+            foreach (var f in xlslFiles)
+            {
+                var excel = new ExcelUtility(f);
+                var json = excel.GetJson();
+                var statements = excel.GetLine(0);
+                Json2Class(f, json, statements);
+                //Json2Class(f, json, statements , true);
+            }
+
+            AssetDatabase.Refresh();
+        }
+
+
+        private static void Json2Class(string fileName, string json, List<object> statements , bool isForSql =false)
+        {
+            string className = "";
+            if (isForSql)
+            {
+              className =   Path.GetFileName(fileName).ToLower().Replace(".xlsx", "_SQL");
+            }
+            else
+            {
+              className =    Path.GetFileName(fileName).ToLower().Replace(".xlsx", "");
+            }
+            string outputFile = "";
+            if (isForSql)
+            {
+               outputFile =  fileName.Replace(".xlsx", "_SQL.cs");
+            }
+            else
+            {
+               outputFile =  fileName.Replace(".xlsx", ".cs");
+            }
+
+            //生成类服务
+            CodeCompileUnit compunit = new CodeCompileUnit();
+            CodeNamespace sample = new CodeNamespace("Game.Data");
+            compunit.Namespaces.Add(sample);
+            //引用命名空间
+            sample.Imports.Add(new CodeNamespaceImport("System"));
+            sample.Imports.Add(new CodeNamespaceImport("System.Collections.Generic"));
+            sample.Imports.Add(new CodeNamespaceImport("Game.Data"));
+            //在命名空间下添加一个类
+            CodeTypeDeclaration wrapProxyClass = new CodeTypeDeclaration(className);
+            //把这个类添加到命名空间 
+            sample.Types.Add(wrapProxyClass);
+            wrapProxyClass.BaseTypes.Add(new CodeTypeReference("LocalDataBase"));
+
+            //
+            var jsonData = JsonMapper.ToObject(json)[0];
+
+            int i = 0;
+            foreach (var key in jsonData.Keys)
+            {
+                if (key.ToLower() == "id"  &&  key != "Id")
+                {
+                    
+                    Debug.LogErrorFormat("<color=yellow>表格{0}字段必须为Id[大小写],请修改后生成</color>" , className);
+                    break;
+                }
+                else if (key == "Id")
+                {
+                    i++;
+                    continue;
+                }
+                var value = jsonData[key];
+
+                //字段
+                CodeMemberField field = new CodeMemberField();
+                field.Attributes = MemberAttributes.Private;
+                //属性
+                CodeMemberProperty property = new CodeMemberProperty();
+                property.Attributes = MemberAttributes.Public | MemberAttributes.Final;;
+                
+                CodeTypeReference type = null;
+                if (value.IsArray)
+                {
+                    if (isForSql)
+                    {
+                       type = new CodeTypeReference(typeof(string)); 
+                    }
+                    else
+                    {
+                        var str = value.ToJson();
+                        if (str.IndexOf("\"") > 0)
+                        {
+                            type = new CodeTypeReference(typeof(List<string>));
+                        }
+                        else
+                        {
+                            type = new CodeTypeReference(typeof(List<double>));
+                        }
+                    }
+
+
+                }
+                else if (value.IsInt) type = new CodeTypeReference(typeof(int));
+                else if (value.IsDouble || value.IsLong) type = new CodeTypeReference(typeof(double));
+                else if (value.IsBoolean) type = new CodeTypeReference(typeof(bool));
+                else if (value.IsString) type = new CodeTypeReference(typeof(string));
+                
+                 //字段
+                field.Type = type;
+                field.Name = "_" + key;
+                //属性
+                property.Type = type;
+                property.Name = key;
+                property.HasGet = true;
+                property.HasSet = true;
+                
+                property.GetStatements.Add(new CodeMethodReturnStatement(new CodeFieldReferenceExpression(new CodeThisReferenceExpression(),  field.Name)));
+                property.SetStatements.Add(new CodeAssignStatement(new CodeFieldReferenceExpression(new CodeThisReferenceExpression(), field.Name), new CodePropertySetValueReferenceExpression()));
+
+                property.Comments.Add(new CodeCommentStatement(statements[i].ToString()));
+
+                //
+                wrapProxyClass.Members.Add(field);
+                wrapProxyClass.Members.Add(property);
+                i++;
+            }
+
+            //生成代码       
+            CodeDomProvider provider = CodeDomProvider.CreateProvider("CSharp");
+            CodeGeneratorOptions options = new CodeGeneratorOptions();
+            options.BracingStyle = "C";
+            options.BlankLinesBetweenMembers = true;
+
+            using (System.IO.StreamWriter sw = new System.IO.StreamWriter(outputFile))
+            {
+
+                provider.GenerateCodeFromCompileUnit(compunit, sw, options);
+
+            }
+        }
+
+    }
+}
