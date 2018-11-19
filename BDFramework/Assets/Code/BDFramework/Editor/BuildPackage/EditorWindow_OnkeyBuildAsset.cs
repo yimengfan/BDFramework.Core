92318ad514106656a212f45d6f7ecdb04c497147
xiaofan <755737878@qq.com>
2018-11-19 11:09:10 +0800

    1.修改ios打包策略

--- BDFramework/Assets/Code/BDFramework/Editor/BuildPackage/EditorWindow_OnkeyBuildAsset.cs
+++ BDFramework/Assets/Code/BDFramework/Editor/BuildPackage/EditorWindow_OnkeyBuildAsset.cs
@@ -118,7 +118,7 @@ public class EditorWindow_OnkeyBuildAsset : EditorWindow
                         //1.编译脚本
                         ScriptBiuldTools.GenDllByMono(Application.dataPath,outPath);
                         //2.打包资源
-                        AssetBundleEditorTools.GenAssetBundle("Resource/Runtime/",outPath, BuildTarget.Android);
+                        AssetBundleEditorTools.GenAssetBundle("Resource/Runtime/",outPath, BuildTarget.iOS);
                         //3.打包表格
                         Excel2SQLiteTools.GenSQLite(outPath);
                     }
