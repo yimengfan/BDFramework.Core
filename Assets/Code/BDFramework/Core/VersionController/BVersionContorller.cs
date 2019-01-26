16c61e438a3048465c4caee643bd4a4b22964a1a
B道友 <755737878@qq.com>
2019-01-26 23:32:25 +0800

    1.修改一个新的下载方案，原webclient 下载文件会有bug

--- Assets/Code/BDFramework/Core/VersionController/BVersionContorller.cs
+++ Assets/Code/BDFramework/Core/VersionController/BVersionContorller.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
@@ -7,6 +8,7 @@ using BDFramework.Http;
 using ILRuntime.Runtime.Intepreter;
 using Mono.Cecil;
 using UnityEngine;
+using UnityEngine.Networking;
 
 namespace BDFramework
 {
@@ -92,13 +94,14 @@ namespace BDFramework
                 //下载
                 try
                 {
-                    await client.DownloadFileTaskAsync(sp, lp);
+                   await client.DownloadFileTaskAsync(sp, lp);
                 }
                 catch (Exception e)
                 {
                     BDebug.LogError(sp);
                     onError( e.Message);
                 }
+                
 
                 BDebug.Log("下载成功：" + sp);
                 onProcess(count, list.Count);
@@ -116,6 +119,111 @@ namespace BDFramework
             
         }
 
+        static  public IEnumerator IEStart(string serverConfigPath, string localConfigPath, Action<int, int> onProcess, Action<string> onError)
+        {
+
+            IPath.Combine("", "");
+            var client = HttpMgr.Inst.GetFreeHttpClient();
+            var platform = Utils.GetPlatformPath(Application.platform);
+            //开始下载服务器配置
+            var serverPath = serverConfigPath + "/" + platform + "/" + platform + "_VersionConfig.json";
+            Debug.Log("server:" + serverPath);
+            string serverConfig = "";
+            //下载config
+            {
+                var wr = UnityWebRequest.Get(serverPath);
+                yield return wr.SendWebRequest();
+                if (wr.error == null)
+                {
+                    serverConfig = wr.downloadHandler.text;
+                    BDebug.Log("服务器资源配置:" + serverConfig);
+                }
+                else
+                {
+                    Debug.LogError(wr.error);
+                }
+
+            }
+
+
+            var serverconf = LitJson.JsonMapper.ToObject<AssetConfig>(serverConfig);
+            AssetConfig localconf = null;
+            var localPath = localConfigPath + "/" + platform + "/" + platform + "_VersionConfig.json";
+
+            if (File.Exists(localPath))
+            {
+                localconf = LitJson.JsonMapper.ToObject<AssetConfig>(File.ReadAllText(localPath));
+            }
+
+            //对比差异列表进行下载
+            var list = CompareConfig(localConfigPath, localconf, serverconf);
+            if (list.Count > 0)
+            {
+                //预通知要进入热更模式
+                onProcess(0, list.Count);
+            }
+
+            int count = 0;
+            foreach (var item in list)
+            {
+                count++;
+                var sp = serverConfigPath + "/" + platform + "/" + item.HashName;
+                var lp = localConfigPath + "/" + platform + "/" + item.LocalPath;
+
+                //创建目录
+                var direct = Path.GetDirectoryName(lp);
+                if (Directory.Exists(direct) == false)
+                {
+                    Directory.CreateDirectory(direct);
+                }
+
+                //下载
+                var wr = UnityWebRequest.Get(sp);
+                yield return wr.SendWebRequest();
+                if (wr.error == null)
+                {
+                    File.WriteAllBytes(lp, wr.downloadHandler.data);
+                    BDebug.Log("下载成功：" + sp);
+                    onProcess(count, list.Count);
+                }
+                else
+                {
+                    BDebug.LogError("下载失败:" + wr.error);
+                    onError(wr.error);
+                }
+            }
+
+            //写到本地
+            if (list.Count > 0)
+            {
+                File.WriteAllText(localPath, serverConfig);
+            }
+            else
+            {
+                BDebug.Log("可更新数量为0");
+            }
+            
+        }
+        
+        
+        
+        
+
+        static IEnumerator  IE_DownloadFile(string serverPath, string localPath, Action<bool> callback)
+        {
+            var wr = UnityWebRequest.Get(serverPath);
+            yield return wr;
+            if (wr.error == null)
+            {
+                File.WriteAllBytes(localPath, wr.downloadHandler.data);
+                callback(true);
+            }
+            else
+            {
+                callback(false);
+            }
+
+        }
 
         /// <summary>
         /// 对比
--- Assets/Code/Game@main/Windows_PreConfig.cs
+++ Assets/Code/Game@main/Windows_PreConfig.cs
@@ -9,20 +9,18 @@ using UnityEngine.UI;
 /// 主工程部分ui直接放在Resources下
 /// 主工程所有代码 都是以M开头，区分热更工程中，避免引用错误
 /// </summary>
-[M_UI(1,"UI/MainLoading")]
+[M_UI(1, "UI/MainLoading")]
 public class Windows_PreConfig : M_AWindow
 {
-    [M_TransformPath("InputField")]
-    private InputField inputField;
-    
+    [M_TransformPath("InputField")] private InputField inputField;
+
     [M_TransformPath("text_DownloadProcess")]
     private Text text_DownloadProcess;
-        
-    [M_TransformPath("btn_Download")]
-    private Button btn_Download;
-    
-    [M_TransformPath("btn_Pass")]
-    private Button btn_Pass;
+
+    [M_TransformPath("btn_Download")] private Button btn_Download;
+
+    [M_TransformPath("btn_Pass")] private Button btn_Pass;
+
     //
     public Windows_PreConfig(string path) : base(path)
     {
@@ -36,12 +34,11 @@ public class Windows_PreConfig : M_AWindow
     public override void Init()
     {
         base.Init();
-        
+
         this.btn_Pass.onClick.AddListener(Onclick_Pass);
         this.btn_Download.onClick.AddListener(Onclick_DownLoad);
         inputField.text = "127.0.0.1";
     }
-
 
 
     void Onclick_Pass()
@@ -53,36 +50,28 @@ public class Windows_PreConfig : M_AWindow
 
     private void Onclick_DownLoad()
     {
-   
-      //删除本地的文件
-      var cachedir =  IPath.Combine( Application.persistentDataPath,Utils.GetPlatformPath(Application.platform));
-      if (Directory.Exists(cachedir))
-      {
-          Directory.Delete(cachedir,true);
-      }
+        //删除本地的文件
+        var cachedir = IPath.Combine(Application.persistentDataPath, Utils.GetPlatformPath(Application.platform));
+        if (Directory.Exists(cachedir))
+        {
+            Directory.Delete(cachedir, true);
+        }
 
-    
+
         var url = "http://" + this.inputField.text;
 
-        VersionContorller.Start(url, Application.persistentDataPath,
-        (i, j) =>
-        {
-            this.text_DownloadProcess.text = string.Format("{0}/{1}", i, j);
-
-            //下载完毕
-            if (i == j)
+        IEnumeratorTool.StartCoroutine(VersionContorller.IEStart(url, Application.persistentDataPath,
+            (i, j) =>
             {
-                this.text_DownloadProcess.text = "下载完毕";
-                //启动
-                GameObject.Find("BDFrame").GetComponent<BDLauncher>().Launch();
-            }
-        },
-        (e) =>
-        {
-            this.text_DownloadProcess.text = e;
-            
-        });
+                this.text_DownloadProcess.text = string.Format("{0}/{1}", i, j);
+                //下载完毕
+                if (i == j)
+                {
+                    this.text_DownloadProcess.text = "下载完毕";
+                    //启动
+                    GameObject.Find("BDFrame").GetComponent<BDLauncher>().Launch();
+                }
+            },
+            (e) => { this.text_DownloadProcess.text = e; }));
     }
-
-
 }
