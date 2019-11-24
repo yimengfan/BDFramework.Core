4a32ab61af78ff59b5edd90370c5e6b07b5289df
奶泡泡 <755737878@qq.com>
2019-11-24 23:46:17 +0800

    1.add hotfix testrunner

---
+++ Assets/Code/BDFramework.Test/@hotfix/TestResult.cs
@@ -0,0 +1,8 @@
+﻿namespace BDFramework.Test.@hotfix
+{
+    public class TestResult
+    {
+        public bool IsPass;
+        public string ErrorMsg = "";
+    }
+}
---
+++ Assets/Code/BDFramework.Test/@hotfix/TestRunner.cs
@@ -0,0 +1,20 @@
+﻿namespace BDFramework.Test.hotfix
+{
+    //所有的Result
+    public delegate void TestCall(TestResult result);
+ 
+    /// <summary>
+    /// 执行所有的runner
+    /// </summary>
+    public class TestRunner
+    {
+        /// <summary>
+        /// 执行所有的TestRunner
+        /// </summary>
+        static public void RunAll()
+        {
+            
+        }
+        
+    }
+}
