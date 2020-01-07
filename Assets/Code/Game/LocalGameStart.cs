dc3390e158498942e10d721cebf367e2a2a1c701
奶泡泡 <755737878@qq.com>
2020-01-07 18:11:23 +0800

    1.删除main工程的傻逼设计

--- Assets/Code/Game/LocalGameStart.cs
+++ Assets/Code/Game/LocalGameStart.cs
@@ -1,8 +1,4 @@
-using System.Reflection;
-using BDFramework;
 using BDFramework.GameStart;
-using Game.UI;
-using UnityEngine;
 
 namespace Game
 {
@@ -12,20 +8,6 @@ namespace Game
         public void Start()
         {
             BDebug.Log("主工程代码启动!","red");
-
-            //初始化M_UIMgr
-            var types = Assembly.GetExecutingAssembly().GetTypes();
-            foreach (var t in types)
-            {
-                M_UIManager.Inst.CheckType(t);
-            }
-            M_UIManager.Inst.Init();
-
-            
-            //加载并显示1号窗口
-            M_UIManager.Inst.LoadWindows(1);
-            M_UIManager.Inst.ShowWindow(1);
-
         }
 
         public void Update()
---
+++ Assets/Code/Game/WindowPreconfig.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.IO;
+using BDFramework;
+using BDFramework.Helper;
+using BDFramework.VersionContrller;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WindowPreconfig : MonoBehaviour
+{
+    private InputField inputField;
+    private Text       text_DownloadProcess;
+    private Button     btn_Download;
+    private Button     btn_Pass;
+
+
+    /// <summary>
+    /// 开始
+    /// </summary>
+    void Start()
+    {
+        //节点发现
+        inputField           = this.transform.Find("InputField").GetComponent<InputField>();
+        text_DownloadProcess = this.transform.Find("text_DownloadProcess").GetComponent<Text>();
+        btn_Download         = this.transform.Find("btn_Download").GetComponent<Button>();
+        btn_Pass             = this.transform.Find("btn_Pass").GetComponent<Button>();
+        //
+        this.btn_Pass.onClick.AddListener(Onclick_PassAndLaunch);
+        this.btn_Download.onClick.AddListener(Onclick_DownLoadAndLaunch);
+        inputField.text = "127.0.0.1";
+    }
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    void Onclick_PassAndLaunch()
+    {
+        //直接启动
+        BDLauncher.Inst.Launch();
+        //
+        this.StartCoroutine(IE_Destroy());
+    }
+
+
+
+
+    private void Onclick_DownLoadAndLaunch()
+    {
+        //删除本地的文件
+        var cachedir = IPath.Combine(Application.persistentDataPath, BDUtils.GetPlatformPath(Application.platform));
+        if (Directory.Exists(cachedir))
+        {
+            Directory.Delete(cachedir, true);
+        }
+
+        var url = "http://" + this.inputField.text;
+        VersionContorller.Start(UpdateMode.Repair, url, Application.persistentDataPath, (i, j) =>
+        {
+            this.text_DownloadProcess.text = string.Format("{0}/{1}", i, j);
+            //下载完毕
+            if (i == j)
+            {
+                this.text_DownloadProcess.text = "下载完毕";
+                //启动
+                GameObject.Find("BDFrame").GetComponent<BDLauncher>().Launch();
+            }
+        }, (e) =>
+        {
+            this.text_DownloadProcess.text = e;
+        });
+    }
+    
+    
+    /// <summary>
+    /// 删除
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator IE_Destroy()
+    {
+        yield return  new WaitForSeconds(3);
+        Destroy(this.gameObject);
+    }
+
+}
