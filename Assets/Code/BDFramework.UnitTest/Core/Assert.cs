e6ec5b1a445d2fcda14a534420bac7466b05f2e6
xiaofan <755737878@qq.com>
2020-04-20 04:07:46 +0800

    1.add testrunner for ILRuntime

--- Assets/Code/BDFramework.UnitTest/Core/Assert.cs
+++ Assets/Code/BDFramework.UnitTest/Core/Assert.cs
@@ -28,6 +28,26 @@ namespace BDFramework.UnitTest
         }
 
         /// <summary>
+        /// 是否为True
+        /// </summary>
+        /// <returns></returns>
+        static public bool IsTrue(bool b, string failMessage = "Fail")
+        {
+            var ret = b;
+            Log(ret, failMessage);
+            return ret;
+        }
+        /// <summary>
+        /// 是否为false
+        /// </summary>
+        /// <returns></returns>
+        static public bool IsFalse(bool b, string failMessage = "Fail")
+        {
+            var ret = b == false;
+            Log(ret, failMessage);
+            return ret;
+        }
+        /// <summary>
         /// 比较值
         /// </summary>
         /// <param name="obj"></param>
@@ -44,12 +64,16 @@ namespace BDFramework.UnitTest
         /// <summary>
         /// log
         /// </summary>
-        /// <param name="ret"></param>
+        /// <param name="ret"> true=成功，false=失败</param>
         /// <exception cref="Exception"></exception>
         static void Log(bool ret, string failMessage = "")
         {
             if (ILRuntimeHelper.IsRunning)
             {
+                if (!ret)
+                {
+                    throw new Exception(failMessage);
+                }
             }
             else
             {
---
+++ Assets/Code/BDFramework.UnitTest/Test@hotfix/APITest_ILRuntime.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using BDFramework.UnitTest;
+using Code.Game;
+using UnityEngine;
+
+namespace Tests
+{
+    [UnitTestAttribute(Des = "框架流程测试")]
+    static public class APITest_ILRuntime
+    {
+
+
+        /// <summary>
+        /// 测试启动逻辑
+        /// </summary>
+        [HotfixUnitTest(Des = "测试Map tryget 为null")]
+        public static void MapTryGetTest()
+        {
+            Dictionary<string,object> testmap = new Dictionary<string, object>();
+            testmap["t"] = null;
+
+            object o =  "str";
+            if (testmap.TryGetValue("t", out o))
+            {
+                int test = 110;
+            }
+            Assert.IsTrue(o==null,"map TryGet API Wrong");
+        }
+    }
+}
