0333598d775c54cce7aeddd3c5b8a4e800d69055
奶泡泡 <y755737878@outlook.com>
2021-04-07 12:54:07 +0800

    【Editor】 change default config

--- Packages/BDFramework/Editor/Config/BDEditorConfig.cs
+++ Packages/BDFramework/Editor/Config/BDEditorConfig.cs
@@ -117,9 +117,9 @@ namespace BDFramework.Editor
         [LabelText("是否hash命名")]
         public bool IsUseHashName = false;
         [LabelText("上传接口")]
-        public string AssetBundleFileServerUrl = "http://127.0.0.1:20000";
+        public string AssetBundleFileServerUrl = "http://127.0.0.1:20001";
 
         [LabelText("自动生成热更DLL")]
-        public bool IsAutoBuildDll = false;
+        public bool IsAutoBuildDll = true;
     }
 }
