bac150e3bcc99e620bef4058d13355f345690f49
奶泡泡 <y755737878@outlook.com>
2021-04-26 19:20:34 +0800

    1.bind add

--- Assets/Code/BDFramework.Game/ILRuntime/Binding/Analysis/System_Collections_Generic_List_1_Int32_Bind.cs
+++ Assets/Code/BDFramework.Game/ILRuntime/Binding/Analysis/System_Collections_Generic_List_1_Int32_Bind.cs
@@ -34,6 +34,9 @@ namespace ILRuntime.Runtime.Generated
             args = new Type[]{};
             method = type.GetMethod("GetEnumerator", flag, null, args, null);
             app.RegisterCLRMethodRedirection(method, GetEnumerator_3);
+            args = new Type[]{typeof(System.Int32)};
+            method = type.GetMethod("RemoveAt", flag, null, args, null);
+            app.RegisterCLRMethodRedirection(method, RemoveAt_4);
 
             args = new Type[]{};
             method = type.GetConstructor(flag, null, args, null);
@@ -110,6 +113,24 @@ namespace ILRuntime.Runtime.Generated
             var result_of_this_method = instance_of_this_method.GetEnumerator();
 
             return ILIntepreter.PushObject(__ret, __mStack, result_of_this_method);
+        }
+
+        static StackObject* RemoveAt_4(ILIntepreter __intp, StackObject* __esp, IList<object> __mStack, CLRMethod __method, bool isNewObj)
+        {
+            ILRuntime.Runtime.Enviorment.AppDomain __domain = __intp.AppDomain;
+            StackObject* ptr_of_this_method;
+            StackObject* __ret = ILIntepreter.Minus(__esp, 2);
+
+            ptr_of_this_method = ILIntepreter.Minus(__esp, 1);
+            System.Int32 @index = ptr_of_this_method->Value;
+
+            ptr_of_this_method = ILIntepreter.Minus(__esp, 2);
+            System.Collections.Generic.List<System.Int32> instance_of_this_method = (System.Collections.Generic.List<System.Int32>)typeof(System.Collections.Generic.List<System.Int32>).CheckCLRTypes(StackObject.ToObject(ptr_of_this_method, __domain, __mStack));
+            __intp.Free(ptr_of_this_method);
+
+            instance_of_this_method.RemoveAt(@index);
+
+            return __ret;
         }
 
 
