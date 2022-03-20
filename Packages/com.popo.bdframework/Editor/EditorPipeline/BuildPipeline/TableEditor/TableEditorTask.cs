01ca7ec118b66485db7a6c3fbd4b6179b7aca836
奶泡泡 <y755737878@outlook.com>
2022-03-20 23:43:57 +0800

    -Add import changed excel on enterplay mode.

---
+++ Packages/com.popo.bdframework/Editor/EditorPipeline/BuildPipeline/TableEditor/TableEditorTask.cs
@@ -0,0 +1,42 @@
+﻿using BDFramework.Editor.Task;
+using BDFramework.Sql;
+using UnityEditor;
+using UnityEngine;
+
+namespace BDFramework.Editor.Table
+{
+    /// <summary>
+    /// 编辑器任务
+    /// </summary>
+    public class TableEditorTask
+    {
+        /// <summary>
+        /// 强制导入改变的excel
+        /// </summary>
+        [EditorTask.EditorTaskOnWillEnterPlaymodeAttribute("强制导入修改的Excel")]
+        static public void OnForceImpotChangedExcel()
+        {
+            //判断是否导入设置
+            if (BDEditorApplication.BDFrameWorkFrameEditorSetting.BuildSetting.IsForceImportChangedExcelOnWillEnterPlaymode)
+            {
+                var changedExcelList = ExcelEditorTools.GetChangedExcels();
+                if (changedExcelList.Count > 0)
+                {
+                    BDebug.Log("-----------------强制导入修改的excel文件.begin-----------------", "red");
+
+                    SqliteLoder.LoadLocalDBOnEditor(Application.streamingAssetsPath, Application.platform);
+                    {
+                        //开始导入
+                        foreach (var excel in changedExcelList)
+                        {
+                            var path = AssetDatabase.GUIDToAssetPath(excel);
+                            Excel2SQLiteTools.Excel2SQLite(path, DBType.Local);
+                        }
+                    }
+                    SqliteLoder.Close();
+                    BDebug.Log("-----------------强制导入修改的excel文件.end-----------------", "red");
+                }
+            }
+        }
+    }
+}
