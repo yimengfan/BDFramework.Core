1d8a82faf5a92d655ccc9981be9e8686896f27f7
奶泡泡 <y755737878@outlook.com>
2022-05-01 03:54:18 +0800

    -Manager: remove debug.

--- Packages/com.popo.bdframework/Runtime/ManagerBase/ManagerInstHelper.cs
+++ Packages/com.popo.bdframework/Runtime/ManagerBase/ManagerInstHelper.cs
@@ -29,7 +29,7 @@ namespace BDFramework.Mgr
                 var type = types[i];
                 if (type != null && type.IsClass && (!type.IsAbstract) && typeof(IMgr).IsAssignableFrom(type))
                 {
-                    BDebug.Log("[main]加载管理器-" + type, "green");
+                    // BDebug.Log("[main]加载管理器-" + type, "green");
                     var inst = type.BaseType.GetProperty("Inst", BindingFlags.Static | BindingFlags.Public);
                     if (inst != null)
                     {
