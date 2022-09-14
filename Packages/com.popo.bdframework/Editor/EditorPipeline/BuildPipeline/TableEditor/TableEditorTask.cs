4e45d3e35bf3b39ae95d790865588a1a153d9961
奶泡泡 <y755737878@outlook.com>
2022-09-14 10:39:45 +0800

    -Sqlite：Add sql file check.

--- Packages/com.popo.bdframework/Editor/EditorPipeline/BuildPipeline/TableEditor/TableEditorTask.cs
+++ Packages/com.popo.bdframework/Editor/EditorPipeline/BuildPipeline/TableEditor/TableEditorTask.cs
@@ -1,4 +1,5 @@
-﻿using BDFramework.Core.Tools;
+﻿using System.IO;
+using BDFramework.Core.Tools;
 using BDFramework.Editor.Task;
 using BDFramework.Sql;
 using UnityEditor;
@@ -18,9 +19,11 @@ namespace BDFramework.Editor.Table
         static public void OnForceImpotChangedExcel()
         {
             //判断是否导入设置
-            if (BDEditorApplication.BDFrameworkEditorSetting.BuildSqlSetting.IsForceImportChangedExcelOnWillEnterPlaymode)
+            if (BDEditorApplication.BDFrameworkEditorSetting.BuildSqlSetting
+                .IsForceImportChangedExcelOnWillEnterPlaymode)
             {
-                var (changedExcelList,newEcxcelInfoMap )= ExcelEditorTools.GetChangedExcels();
+                var dbPath = SqliteLoder.GetLocalDBPath(Application.streamingAssetsPath, BApplication.RuntimePlatform);
+                var (changedExcelList, newEcxcelInfoMap) = ExcelEditorTools.GetChangedExcels();
                 if (changedExcelList.Count > 0)
                 {
                     BDebug.Log("-----------------强制导入修改的excel文件.begin-----------------", "red");
@@ -36,9 +39,33 @@ namespace BDFramework.Editor.Table
                     }
                     SqliteLoder.Close();
                     BDebug.Log("-----------------强制导入修改的excel文件.end-----------------", "red");
+
+                    ExcelEditorTools.SaveExcelCacheInfo(newEcxcelInfoMap);
+                    //db_hash
+                    ExcelEditorTools.SaveLocalDBCacheInfo(dbPath);
+                }
+                //通过本地存储hash判断是否需要所有导表
+                var hash = ExcelEditorTools.LoadLocalDBCacheInfo();
+                var curhash = FileHelper.GetMurmurHash3(dbPath);
+                //所有导表
+                if (hash != curhash)
+                {
+                    var excelPathList = ExcelEditorTools.GetAllExcelFiles();
+
+                    SqliteLoder.LoadLocalDBOnEditor(Application.streamingAssetsPath, BApplication.RuntimePlatform);
+                    {
+                        //开始导入
+                        foreach (var excelPath in excelPathList)
+                        {
+                            Excel2SQLiteTools.Excel2SQLite(excelPath, DBType.Local);
+                        }
+                    }
+                    SqliteLoder.Close();
+
+                    BDebug.Log("-----------------强制导入修改的excel文件.end-----------------", "red");
+                    ExcelEditorTools.SaveLocalDBCacheInfo(dbPath);
                 }
                 //保存配置
-                ExcelEditorTools.SaveExcelCacheInfo(newEcxcelInfoMap);
             }
         }
     }
