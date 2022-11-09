e3d16b33c5dc0e54544c9ffc73a93542a1b1f2d4
奶泡泡 <y755737878@outlook.com>
2022-11-09 18:24:10 +0800

    -修复Setting impotring异常报错

--- Packages/com.popo.bdframework/3rdPlugins/AssetGraph-1.8-release-BD/Editor/System/Importers/TextureImportSettingsConfigurator.cs
+++ Packages/com.popo.bdframework/3rdPlugins/AssetGraph-1.8-release-BD/Editor/System/Importers/TextureImportSettingsConfigurator.cs
@@ -19,8 +19,8 @@ namespace UnityEngine.AssetGraph
 
         public void Initialize(ConfigurationOption option)
         {
-            m_overwritePackingTag      = option.overwritePackingTag;
-            m_overwriteSpriteSheet     = option.overwriteSpriteSheet;
+            m_overwritePackingTag = option.overwritePackingTag;
+            m_overwriteSpriteSheet = option.overwriteSpriteSheet;
             m_customPackingTagTemplate = option.customPackingTagTemplate;
         }
 
@@ -42,7 +42,12 @@ namespace UnityEngine.AssetGraph
             var t = importer as TextureImporter;
             if (t == null || r == null)
             {
-                throw new AssetGraphException($"Invalid AssetImporter assigned for {importer.assetPath}");
+                if (importer != null)
+                {
+                    Debug.LogError($"Invalid AssetImporter assigned for {importer.assetPath}");
+                }
+
+                return;
             }
 
             OverwriteImportSettings(t, r, GetTagName(target, group));
@@ -73,12 +78,12 @@ GenerateMipMaps
                     GUILayout.Label("Sprite Settings");
                     GUILayout.Space(4f);
                     var bSpriteSheet = EditorGUILayout.ToggleLeft("Configure Sprite Mode", m_overwriteSpriteSheet);
-                    var bPackingTag  = EditorGUILayout.ToggleLeft("Configure Sprite Packing Tag", m_overwritePackingTag);
+                    var bPackingTag = EditorGUILayout.ToggleLeft("Configure Sprite Packing Tag", m_overwritePackingTag);
 
                     if (bSpriteSheet != m_overwriteSpriteSheet || bPackingTag != m_overwritePackingTag)
                     {
                         m_overwriteSpriteSheet = bSpriteSheet;
-                        m_overwritePackingTag  = bPackingTag;
+                        m_overwritePackingTag = bPackingTag;
                         onValueChanged();
                     }
 
@@ -143,21 +148,21 @@ GenerateMipMaps
 
             if (reference == null || target.textureType != reference.textureType) return false;
 
-            TextureImporterSettings targetSetting    = new TextureImporterSettings();
+            TextureImporterSettings targetSetting = new TextureImporterSettings();
             TextureImporterSettings referenceSetting = new TextureImporterSettings();
 
             target.ReadTextureSettings(targetSetting);
             reference.ReadTextureSettings(referenceSetting);
 
 
-            if (targetSetting.aniso                              != referenceSetting.aniso) return false;
-            if (targetSetting.compressionQuality                 != referenceSetting.compressionQuality) return false;
+            if (targetSetting.aniso != referenceSetting.aniso) return false;
+            if (targetSetting.compressionQuality != referenceSetting.compressionQuality) return false;
             if (targetSetting.spriteGenerateFallbackPhysicsShape != referenceSetting.spriteGenerateFallbackPhysicsShape) return false;
-            if (targetSetting.spritePixelsPerUnit                != referenceSetting.spritePixelsPerUnit) return false;
-            if (targetSetting.npotScale                          != referenceSetting.npotScale) return false;
-            if (targetSetting.readable                           != referenceSetting.readable) return false;
-            if (targetSetting.streamingMipmapsPriority           != referenceSetting.streamingMipmapsPriority) return false;
-            if (targetSetting.mipmapEnabled                      != referenceSetting.mipmapEnabled) return false;
+            if (targetSetting.spritePixelsPerUnit != referenceSetting.spritePixelsPerUnit) return false;
+            if (targetSetting.npotScale != referenceSetting.npotScale) return false;
+            if (targetSetting.readable != referenceSetting.readable) return false;
+            if (targetSetting.streamingMipmapsPriority != referenceSetting.streamingMipmapsPriority) return false;
+            if (targetSetting.mipmapEnabled != referenceSetting.mipmapEnabled) return false;
             if (m_overwritePackingTag)
             {
                 if (target.spritePackingTag != reference.spritePackingTag) return false;
@@ -165,14 +170,14 @@ GenerateMipMaps
 
 
             if (target.androidETC2FallbackOverride != reference.androidETC2FallbackOverride) return false;
-            if (target.compressionQuality          != reference.compressionQuality) return false;
-            if (target.crunchedCompression         != reference.crunchedCompression) return false;
-            if (target.isReadable                  != reference.isReadable) return false;
-            if (target.npotScale                   != reference.npotScale) return false;
-            if (target.streamingMipmaps            != reference.streamingMipmaps) return false;
-            if (target.streamingMipmapsPriority    != reference.streamingMipmapsPriority) return false;
-            if (target.textureCompression          != reference.textureCompression) return false;
-            if (target.mipmapEnabled               != reference.mipmapEnabled) return false;
+            if (target.compressionQuality != reference.compressionQuality) return false;
+            if (target.crunchedCompression != reference.crunchedCompression) return false;
+            if (target.isReadable != reference.isReadable) return false;
+            if (target.npotScale != reference.npotScale) return false;
+            if (target.streamingMipmaps != reference.streamingMipmaps) return false;
+            if (target.streamingMipmapsPriority != reference.streamingMipmapsPriority) return false;
+            if (target.textureCompression != reference.textureCompression) return false;
+            if (target.mipmapEnabled != reference.mipmapEnabled) return false;
 
 
             var refDefault = reference.GetDefaultPlatformTextureSettings();
@@ -183,7 +188,7 @@ GenerateMipMaps
             {
                 var platformName = BuildTargetUtility.TargetToAssetBundlePlatformName(g, BuildTargetUtility.PlatformNameType.TextureImporter);
 
-                var impSet       = reference.GetPlatformTextureSettings(platformName);
+                var impSet = reference.GetPlatformTextureSettings(platformName);
                 var targetImpSet = target.GetPlatformTextureSettings(platformName);
                 if (!CompareImporterPlatformSettings(impSet, targetImpSet)) return false;
             }
@@ -196,34 +201,34 @@ GenerateMipMaps
         {
             target.textureType = reference.textureType;
 
-            var targetSetting    = new TextureImporterSettings();
+            var targetSetting = new TextureImporterSettings();
             var referenceSetting = new TextureImporterSettings();
 
             target.ReadTextureSettings(targetSetting);
             reference.ReadTextureSettings(referenceSetting);
 
             //以下都是被锁定的图片设置
-            targetSetting.aniso                              = referenceSetting.aniso;
-            targetSetting.compressionQuality                 = referenceSetting.compressionQuality;
+            targetSetting.aniso = referenceSetting.aniso;
+            targetSetting.compressionQuality = referenceSetting.compressionQuality;
             targetSetting.spriteGenerateFallbackPhysicsShape = referenceSetting.spriteGenerateFallbackPhysicsShape;
-            targetSetting.spritePixelsPerUnit                = referenceSetting.spritePixelsPerUnit;
-            targetSetting.npotScale                          = referenceSetting.npotScale;
-            targetSetting.readable                           = referenceSetting.readable;
-            targetSetting.streamingMipmapsPriority           = referenceSetting.streamingMipmapsPriority;
-            targetSetting.mipmapEnabled                      = referenceSetting.mipmapEnabled;
+            targetSetting.spritePixelsPerUnit = referenceSetting.spritePixelsPerUnit;
+            targetSetting.npotScale = referenceSetting.npotScale;
+            targetSetting.readable = referenceSetting.readable;
+            targetSetting.streamingMipmapsPriority = referenceSetting.streamingMipmapsPriority;
+            targetSetting.mipmapEnabled = referenceSetting.mipmapEnabled;
 
             target.SetTextureSettings(targetSetting);
 
             target.androidETC2FallbackOverride = reference.androidETC2FallbackOverride;
-            target.anisoLevel                  = reference.anisoLevel;
-            target.compressionQuality          = reference.compressionQuality;
-            target.crunchedCompression         = reference.crunchedCompression;
-            target.isReadable                  = reference.isReadable;
-            target.npotScale                   = reference.npotScale;
-            target.streamingMipmapsPriority    = reference.streamingMipmapsPriority;
-            target.streamingMipmaps            = reference.streamingMipmaps;
-            target.textureCompression          = reference.textureCompression;
-            target.mipmapEnabled               = reference.mipmapEnabled;
+            target.anisoLevel = reference.anisoLevel;
+            target.compressionQuality = reference.compressionQuality;
+            target.crunchedCompression = reference.crunchedCompression;
+            target.isReadable = reference.isReadable;
+            target.npotScale = reference.npotScale;
+            target.streamingMipmapsPriority = reference.streamingMipmapsPriority;
+            target.streamingMipmaps = reference.streamingMipmaps;
+            target.textureCompression = reference.textureCompression;
+            target.mipmapEnabled = reference.mipmapEnabled;
 
             if (m_overwritePackingTag)
             {
@@ -243,23 +248,23 @@ GenerateMipMaps
             foreach (var g in NodeGUIUtility.SupportedBuildTargetGroups)
             {
                 var platformName = BuildTargetUtility.TargetToAssetBundlePlatformName(g, BuildTargetUtility.PlatformNameType.TextureImporter);
-                var impSet       = reference.GetPlatformTextureSettings(platformName);
+                var impSet = reference.GetPlatformTextureSettings(platformName);
                 target.SetPlatformTextureSettings(impSet);
             }
         }
 
         bool CompareImporterPlatformSettings(TextureImporterPlatformSettings c1, TextureImporterPlatformSettings c2)
         {
-            if (c1.allowsAlphaSplitting        != c2.allowsAlphaSplitting) return false;
+            if (c1.allowsAlphaSplitting != c2.allowsAlphaSplitting) return false;
             if (c1.androidETC2FallbackOverride != c2.androidETC2FallbackOverride) return false;
-            if (c1.compressionQuality          != c2.compressionQuality) return false;
-            if (c1.crunchedCompression         != c2.crunchedCompression) return false;
-            if (c1.format                      != c2.format) return false;
-            if (c1.maxTextureSize              != c2.maxTextureSize) return false;
-            if (c1.name                        != c2.name) return false;
-            if (c1.overridden                  != c2.overridden) return false;
-            if (c1.resizeAlgorithm             != c2.resizeAlgorithm) return false;
-            if (c1.textureCompression          != c2.textureCompression) return false;
+            if (c1.compressionQuality != c2.compressionQuality) return false;
+            if (c1.crunchedCompression != c2.crunchedCompression) return false;
+            if (c1.format != c2.format) return false;
+            if (c1.maxTextureSize != c2.maxTextureSize) return false;
+            if (c1.name != c2.name) return false;
+            if (c1.overridden != c2.overridden) return false;
+            if (c1.resizeAlgorithm != c2.resizeAlgorithm) return false;
+            if (c1.textureCompression != c2.textureCompression) return false;
 
             return true;
         }
