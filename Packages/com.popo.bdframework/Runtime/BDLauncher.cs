38a9378afff84ab6b382c61d9bf47bb97f641251
奶泡泡 <y755737878@outlook.com>
2023-03-10 23:27:10 +0800

    -Publish 2.2.0-preview.1

--- Packages/com.popo.bdframework/Runtime/BDLauncher.cs
+++ Packages/com.popo.bdframework/Runtime/BDLauncher.cs
@@ -23,7 +23,7 @@ namespace BDFramework
         /// <summary>
         /// 框架版本号
         /// </summary>
-        public const string Version  = "2.1.0";
+        public const string Version  = "2.2.0-preview.1";
 
         /// <summary>
         /// 客户端配置信息
