be164b537c7a1a6a87016940e17061bade45cbc0
奶泡泡 <y755737878@gmail.com>
2024-02-04 01:49:58 +0800

    修复BtnOnClick

---
+++ Packages/com.popo.bdframework/Runtime/UI(UFlux)/@hotfix/View/Atrribute/AutoInitComponent/ButtonOnclickAttribute.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BDFramework.UFlux
+{
+    /// <summary>
+    /// 自动初始化，按钮点击注册属性
+    /// </summary>
+    public class ButtonOnclickAttribute : AutoInitComponentAttribute
+    {
+        private string path;
+
+        private bool isTriggerThisOnly = false;
+        public ButtonOnclickAttribute(string path, bool isTriggerThisOnly = true)
+        {
+            this.path = path;
+            this.isTriggerThisOnly = isTriggerThisOnly;
+        }
+
+        public override void AutoSetMethod(IComponent com, MethodInfo methodInfo)
+        {
+  
+            Action action = null;
+
+
+            var btn = com.Transform.Find(this.path)?.GetComponent<Button>();
+            if (btn)
+            {
+                if (isTriggerThisOnly)
+                {
+                    btn.onClick.RemoveAllListeners();
+                }
+                btn.onClick.AddListener(() =>
+                {
+                    //触发按钮事件
+                    methodInfo.Invoke(com, new object[] { });
+                });
+            }
+            else
+            {
+                throw  new Exception("未找到Btn:" + this.path);
+            }
+        }
+    }
+}
